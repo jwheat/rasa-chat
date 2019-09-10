f08b33bf3cf1258a93a63b19e5a54c5bbdec48de
Jonathan Wheat <wheat@manadatechnology.com>
2019-09-10 12:14:33 -0400

    form problem

---
+++ actions.py
@@ -0,0 +1,51 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message("Hello World!")
+#
+#         return []
+
+class NameForm(FormAction):
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+        return "name_form"
+    
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["name"]
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do after all slots are filled"""
+
+        #utter submti template
+        dispatcher.utter_template("utter_submit", tracker)
+        return[]
